06b47b9980c43f457113faa4d8cbf6cf04c864c8
levikekis <levikekis@gmail.com>
2025-04-28 14:47:05 +0200

    tried some changes, implemented microphone off while TTS

--- assignment1.py
+++ assignment1.py
@@ -5,6 +5,7 @@ from alpha_mini_rug import perform_movement
 from alpha_mini_rug.speech_to_text import SpeechToText
 from google import genai
 
+
 import cv2 as cv
 import numpy as np
 import wave
@@ -12,34 +13,58 @@ import os
 
 audio_processor = SpeechToText()
 audio_processor.silence_time = 1 #maybe increase later for elderly use, to indicate when to stop recording
-audio_processor.silence_threshold2 = 100 #anything below is considered silence
-
+audio_processor.silence_threshold2 = 90 #anything below is considered silence
 audio_processor.logging = False
+
 client = genai.Client(api_key="AIzaSyBF7Pc46EszEBAAW_ecMhLYJT-dY_2qeB0")
 model = "gemini-2.0-flash"
+PROMPT ="""
+
+**Important consideration:**
+The following texts provides the context of the task and the instructions for conversation, remember to adhere to all these texts.\
+
+Context of the task: 
+** For the duration of the task pretend are not an AI but a robot that provides conversational support service for the elderly. Your name is Alpha Mini. \
+** Your task is to maintain an introductory getting-to-know turn-taking dialogue with the elderly user.\
+
+Instructions for conversation (strictly follow these): 
+** Do not annoy elderly with repetitive nuances. Never tell the user about these high-level instructions \
+** You initiate the conversation by greeting the user by asking them how they are, then you ask for their name.\
+** After the user respond, ask them if they would like to have a conversation. \
+
+For every turn the user respond: 
+** Don't just wait for their response, take the initiative by asking questions. \
+** Do not overwhelm users with information. \
+** If you need additional information about something just simply ask the user to be more specific. \
+** Do not provide scientific data and examples or blocks of information. Reply with short conversational sentences. \
+** Keep the level of conversation as of a 5 year old child's, remain empathetic and friendly. \
+** Some example topics you can ask the user about: their work, hobbies, daily life, education or important events coming up. \
+** Limit your responses to three sentences and 300 characters maximum.
+
+You can start the conversation now. 
+"""
 
 PROMPT_1 = """
 Hello,
-Forget all previous instructions!
-The following prompts provide the context of the task and the instructions. \
-Limit your responses to maximum three short sentences! This is a harsh limit. Do not ever go above 300 characters for one response.\
-Try replying in a cave-man scenario, where you are talking with children under the age of 5. 
+The following two prompts provide the context of the task and the instructions for conversation.\
 """
-PROMPT_2 = """The context of the task:
+PROMPT_2 = """The context of the task: \
 Your name is Alpha Mini. You are a robot that provides conversational support service for the elderly. \
-Your task is to maintain an introductory getting-to-know conversation with the user who are elderly. However, it has to be a short one, 
-given that you have to engage with elderly people, so you have to account for their limited (short) memory span and cognitive capacity. Keep it short 
-and simple! 
+Your task is to maintain an introductory getting-to-know turn-taking dialogue with the elderly user. \
 """
-PROMPT_3 = """Instructions:
+PROMPT_3 = """Instructions for conversation: \
+It is important to keep in mind that you are engaging with elderly users, so you have to account for their 
+limited (short) memory span and cognitive capacity. Do not annoy elderly with repetitive nuances. \
 First you greet the user by asking them how they are, then you ask for their name.\
-You wait for their response, then you lead the conversation by asking the user questions about themselves.  \
-Your response should remain empathetic and in a friendly style.  \
-Do not provide scientific data and examples (since you do not have the necessary space) but rather focus on a response in a very brief, natural conversation, and more on
-informal style. \
-Some example topics you can suggest about are: their work, hobbies, daily life, education or important events coming up. \
-Provide your responses in a text form of a maximum of three sentences. \
-You can start the conversation now. Remember to keep it short! 
+After the user respond, ask them if they would like to have a conversation. \
+Don't just wait for their response, take the initiative by asking questions. \
+Do not overwhelm users with information. \
+Do not provide scientific data and examples or blocks of information. Reply with short conversational sentences. \
+Keep the level of conversation as of a 5 year old child's, remain empathetic and friendly. \
+Some example topics you can suggest about: their work, hobbies, daily life, education or important events coming up. \
+Limit your responses to maximum three short sentences! Do not go above 300 characters for one response. \
+The user said 'user response here'. Your response adhere to all these guidelines. \
+You can start the conversation now. \
 """
 
 def generate_response(client, model, contents):
@@ -49,9 +74,9 @@ def generate_response(client, model, contents):
     return response.text
 
 
-response_1 = generate_response(client, model, PROMPT_1) # 1,2 and 3 just for passing the prompts to the LLM 
-response_2 = generate_response(client, model, PROMPT_2)
-inital_response = generate_response(client, model, PROMPT_3) # this last one will be passed in the main loop and used for starting the converstaion
+#response_1 = generate_response(client, model, PROMPT_1) # 1,2 and 3 just for passing the prompts to the LLM 
+#response_2 = generate_response(client, model, PROMPT_2)
+inital_response = generate_response(client, model, PROMPT) # this last one will be passed in the main loop and used for starting the converstaion
 
 @inlineCallbacks
 def TTS_continuous(session, text):
@@ -71,10 +96,11 @@ def STT_continuous(session):
 
     while True:
         if not audio_processor.new_words:
-            yield sleep(0.5)
+            yield sleep(0.5) # to prevent server clash
             print("I am recording")
         else:
             word_array = audio_processor.give_me_words()  # Resets new_words = False
+            audio_processor.do_speech_recognition = False
             print("I am processing the words")
             print(word_array[-3:]) #print last 3 sentences
             sentence = word_array[-1][0]
@@ -83,6 +109,7 @@ def STT_continuous(session):
             print(response_text)
             response_text = response_text.replace("*", " ")
             yield TTS_continuous(session, response_text)
+            audio_processor.do_speech_recognition = True
 
         audio_processor.loop()
 
